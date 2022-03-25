82a96cf4568b4d992748e2c512f212732d54f320
Will Cary <40574120+willcary@users.noreply.github.com>
2022-03-25 14:28:54 -0400

    Task: Add FAQLayout component - issue #15 (#17)
    * Create FAQLayout component with styles module
    
    * Destructure children props
    
    * Indent children within FAQLayout component

---
+++ src/components/FAQLayout/FAQLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const FAQLayout = ({ children }) => {
+	return (
+		<div className={styles[`faq-layout`]}>
+            		{children}
+		</div>
+	)
+}
+
+export default FAQLayout;
