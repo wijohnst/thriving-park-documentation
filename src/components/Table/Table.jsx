7b01350631a32972f10a2592acbd4543bf8a5aab
Pedro Cardoso da Silva <forsureitsme@gmail.com>
2022-03-28 20:44:38 +0000

    Refactor Table component
    Remove grid gap and table background
    Add triangle on table leftover space to misdirect border alignments

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-const Table = ({ header = [], body = [] }) => (
-    <div className={styles[`table-wrapper`]} style={{'--columns': header.length}}>
-        {header.map((item, index) => (
-            <div className={styles[`table-header-item`]} key={index}>
-                {item}
-            </div>
-        ))}
-        {body.map((item, index) => (
-            <div className={styles[`table-body-item`]} key={index}>
-                {item}
-            </div>
-        ))}
-    </div>
-)
+const Table = ({ header = [], body = [] }) => {
+
+    const TableLeftoverSpace = () => {
+        const hasLeftoverSpace = body.length % header.length !== 0;
+        
+        if(hasLeftoverSpace) {
+            return (
+                <div className={styles[`table-leftover-space`]} />
+            );
+        }
+
+        return null;
+    };
+
+    return (
+        <div className={styles[`table-wrapper`]} style={{'--columns': header.length}}>
+            {header.map((item, index) => (
+                <div className={styles[`table-header-item`]} key={index}>
+                    {item}
+                </div>
+            ))}
+            {body.map((item, index) => (
+                <div className={styles[`table-body-item`]} key={index}>
+                    {item}
+                </div>
+            ))}
+            <TableLeftoverSpace />
+        </div>
+    )
+}
 
 export default Table;
