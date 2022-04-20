d1462f8df0dd238299784e7c7b37dd2c2a34f91e
willcary <w.a.c.ary@gmail.com>
2022-04-20 10:35:20 -0400

    Remove semicolons for uniformity with codebase.

--- src/components/ParkSign/ParkSign.jsx
+++ src/components/ParkSign/ParkSign.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './styles.module.css';
+import React from 'react'
+import styles from './styles.module.css'
 
 export const ParkSign = () => {
   return (
@@ -12,7 +12,7 @@ export const ParkSign = () => {
         </span>
       </h1>
     </div>
-  );
-};
+  )
+}
 
-export default ParkSign;
+export default ParkSign
