666d761e8c11422dcaa8ad8af204c27ac068978c
willcary <w.a.c.ary@gmail.com>
2022-04-20 10:14:29 -0400

    Refactor ParkSign with semantic html and clamp()

--- src/components/ParkSign/ParkSign.jsx
+++ src/components/ParkSign/ParkSign.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from "react";
+import styles from "./styles.module.css";
 
 export const ParkSign = () => {
-	return (
-		<div className={styles[`park-sign-wrapper`]}>
-			<div className={styles.signContent}>
-				<h2 className={styles[`sign-h2`]}>WELCOME TO</h2>
-				<h1 className={styles[`sign-h1`]}> THRIVING PARK</h1>
-				<h3 className={styles[`sign-h3`]}>COLUMBUS, OHIO - ESTD. 1892</h3>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className={styles[`park-sign-wrapper`]}>
+      <h1 className={styles[`sign-h1`]}>
+        WELCOME TO
+        <span className={styles[`sign-h1-large`]}>THRIVING PARK</span>
+        <span className={styles[`sign-h1-small`]}>
+          COLUMBUS, OHIO - ESTD. 1892
+        </span>
+      </h1>
+    </div>
+  );
+};
 
 export default ParkSign;
